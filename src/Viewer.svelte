f3a6335038728a02a18ecf8850dcfa4fbf5039e3
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2023-04-26 13:26:15 +0200

    change name.

--- src/Viewer.svelte
+++ src/Viewer.svelte
@@ -174,7 +174,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"/>
         </svg>
       </button>
-      <button class="extra-small" on:click={moveForward} disabled={index === questions.length ? 'disabled' : null}>
+      <button class="extra-small" on:click={moveForward} disabled={index + 1 >= questions.length ? 'disabled' : null}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
         </svg>
